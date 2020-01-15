7de6f1367cdb3fa59024827e26d8e664cfee98ae
fazlearabbi16 <fazlearabbi07@gmail.com>
2020-01-16 01:56:20 +0600

    Booklist Seeder Crate

--- database/factories/BooklistFactory.php
+++ database/factories/BooklistFactory.php
@@ -2,13 +2,13 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\booklist;
+use App\Booklists;
 use Faker\Generator as Faker;
 
-$factory->define(booklist::class, function (Faker $faker) {
+$factory->define(Booklists::class, function (Faker $faker) {
     return [
-        "bookname"=>$faker->words($nb = 10, $asText = false),
-        "author"=>$faker->name($gender ='male'|'female'),
-        "body"=>$faker->sentences($nb = 20, $asText = false)
+        "bookname"=>$faker->words($nb = 10),
+        "author"=>$faker->name($gender ='male'),
+        "body"=>$faker->sentences($nb = 20)
     ];
 });
